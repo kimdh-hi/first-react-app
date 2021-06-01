735acaa2b450f74700ade5c02ac3c5df9379fe56
kimdaehyun <zbeld123@gmail.com>
2021-06-02 00:04:36 +0900

    create User model

---
+++ model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const user_model = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+  },
+  role: {
+    type: Number,
+    default: 1, // 1=user, 2=admin
+  },
+  image: String,
+  token: String,
+  token_exp: Number,
+});
+
+const user = mongoose.model("user", user_model);
+
+module.exports = { user };
